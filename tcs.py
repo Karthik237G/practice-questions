ffed5a86209c63592193059a65ade79dc31e94ea
Karthik237G <gollapallikarthik03@gmail.com>
2024-10-04 23:36:02 +0530

    Create tcs.py

---
+++ tcs.py
@@ -0,0 +1,18 @@
+#conversion of des into bin
+def fun(num):
+  res=bin(num)[2:]
+  print(res)
+if __name__=='__main__':
+  num=50
+  fun(50)
+
+#toggling of bin and printing it in int
+#toggling refers to conversion of 1 bit to 0 and vice versa
+def fun(num):
+  res=bin(num)[2:]
+  tog=(''.join("1" if bit=="0" else "0" for bit in res))
+  tog=int(tog,2)
+  print(tog)
+if __name__=='__main__':
+  num=50
+  fun(50)
